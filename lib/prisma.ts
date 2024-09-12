e9e2ea5e469837987ee9e030e08af1842a338b31
TahourDev <72548356+tahourdev@users.noreply.github.com>
2024-09-12 15:07:54 +0700

    update

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -15,9 +15,23 @@
 // if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
 
 import { PrismaClient } from '@prisma/client';
-declare global {
-  var prisma: PrismaClient | undefined;
+
+// Create a singleton instance of PrismaClient
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+// Declare a global namespace for development to store Prisma Client
+declare const globalThis: {
+  prismaGlobal?: PrismaClient;
+} & typeof global;
+
+// Create a single instance of PrismaClient if it does not exist
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+// Assign the instance to globalThis for development environment
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prismaGlobal = prisma;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = db;
+export default prisma;
