5ade6da06575118754616d1777381ce798203f9d
TahourDev <72548356+tahourdev@users.noreply.github.com>
2024-11-14 20:21:33 +0700

    remove sitemap

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 import HomePage from '@/components/homepage/HomePage';
-import Head from 'next/head';
-import Image from "next/image";
 
 // export const experimental_ppr = true
 export default function Home() {
