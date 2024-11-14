6e32e82edffeb22e3e4b5d24e7333e6921b3467e
TahourDev <72548356+tahourdev@users.noreply.github.com>
2024-11-14 20:18:24 +0700

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -5,54 +5,24 @@ import { Metadata } from 'next';
 import BlogContent from './BlogContent';
 import { cache } from 'react';
 
-// export async function generateStaticParams() {
-//     const blogs = await prisma.blog.findMany({
-//         select: { slug: true },
-//     });
-
-//     return blogs.map((blog) => ({
-//         slug: blog.slug,
-//     }));
-// }
-
 export async function generateStaticParams() {
-    try {
-        const blogs = await prisma.blog.findMany({
-            select: { slug: true },
-        });
-        return blogs.map((blog) => ({
-            slug: blog.slug,
-        }));
-    } catch (error) {
-        console.error("Failed to generate static params:", error);
-        return [];
-    }
+    const blogs = await prisma.blog.findMany({
+        select: { slug: true },
+    });
+
+    return blogs.map((blog) => ({
+        slug: blog.slug,
+    }));
 }
 
-
-// const getUniqPost = cache(async (slug: string) => {
-//     const blog = await prisma.blog.findUnique({
-//         where: {
-//             slug,
-//         },
-//     });
-//     return blog;
-// })
-
 const getUniqPost = cache(async (slug: string) => {
-    try {
-        const blog = await prisma.blog.findUnique({
-            where: {
-                slug,
-            },
-        });
-        return blog;
-    } catch (error) {
-        console.error("Error fetching blog post:", error);
-        return null;
-    }
-});
-
+    const blog = await prisma.blog.findUnique({
+        where: {
+            slug,
+        },
+    });
+    return blog;
+})
 
 interface BlogDetailParams {
     slug: string;
@@ -80,66 +50,12 @@ const BlogDetail = async ({ params }: { params: BlogDetailParams }) => {
 
 export default BlogDetail;
 
-// export async function generateMetadata({ params: { slug } }: { params: BlogDetailParams }): Promise<Metadata> {
-//     const blog = await getUniqPost(slug);
-
-//     try {
-//         if (!blog) {
-//             return {};
-//         }
-
-//         return {
-//             title: blog.title,
-//             description: blog.content.slice(0, 150),
-//             twitter: {
-//                 card: "summary_large_image",
-//                 title: blog.title,
-//                 description: blog.content.slice(0, 150),
-//                 images: [blog.image as string],
-//                 creator: "@abandoned_hill",
-//             },
-//             alternates: {
-//                 canonical: `/blog/${blog.slug}`,
-//                 languages: {
-//                     "en-US": `/en-US/${blog.slug}`,
-//                     "de-DE": `/de-DE/${blog.slug}`,
-//                     "km-KH": `/km-KH/${blog.slug}`,
-//                 },
-//             },
-//             robots: {
-//                 index: true,
-//                 follow: true,
-//             },
-//         };
-//     } catch (error) {
-//         return {
-//             title: "Not Found",
-//             description: "The page you are looking for does not exist.",
-//             twitter: {
-//                 card: "summary",
-//                 title: "Not Found",
-//                 description: "The page you are looking for does not exist.",
-//                 creator: "@abandoned_hill",
-//             },
-//         };
-//     }
-// }
-
 export async function generateMetadata({ params: { slug } }: { params: BlogDetailParams }): Promise<Metadata> {
-    try {
-        const blog = await getUniqPost(slug);
+    const blog = await getUniqPost(slug);
 
+    try {
         if (!blog) {
-            return {
-                title: "Blog Not Found",
-                description: "The blog post you're looking for does not exist.",
-                twitter: {
-                    card: "summary",
-                    title: "Not Found",
-                    description: "The page you are looking for does not exist.",
-                    creator: "@abandoned_hill",
-                },
-            };
+            return {};
         }
 
         return {
@@ -166,14 +82,13 @@ export async function generateMetadata({ params: { slug } }: { params: BlogDetai
             },
         };
     } catch (error) {
-        console.error("Error generating metadata:", error);
         return {
-            title: "Error",
-            description: "There was an error loading the page.",
+            title: "Not Found",
+            description: "The page you are looking for does not exist.",
             twitter: {
                 card: "summary",
-                title: "Error",
-                description: "There was an error loading the page.",
+                title: "Not Found",
+                description: "The page you are looking for does not exist.",
                 creator: "@abandoned_hill",
             },
         };
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,39 +1,39 @@
-import prisma from '@/lib/prisma';
-import { MetadataRoute } from 'next';
-import { Blog } from '@/types'; // Import the Blog type
+// import prisma from '@/lib/prisma';
+// import { MetadataRoute } from 'next';
+// import { Blog } from '@/types'; // Import the Blog type
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  // Await the Prisma query directly before mapping
-  const blogs: Blog[] = await prisma.blog.findMany();
+// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+//   // Await the Prisma query directly before mapping
+//   const blogs: Blog[] = await prisma.blog.findMany();
 
-  const blogUrls: MetadataRoute.Sitemap = blogs.map((blog: Blog) => {
-    return {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog/${blog.slug}`,
-      lastModified: blog.updatedAt ? new Date(blog.updatedAt) : new Date(), // Use a fallback value
-    };
-  });
+//   const blogUrls: MetadataRoute.Sitemap = blogs.map((blog: Blog) => {
+//     return {
+//       url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog/${blog.slug}`,
+//       lastModified: blog.updatedAt ? new Date(blog.updatedAt) : new Date(), // Use a fallback value
+//     };
+//   });
 
-  return [
-    {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/about`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/service`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/portfolio`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/contact`,
-      lastModified: new Date(),
-    },
-    ...blogUrls,
-  ];
-}
+//   return [
+//     {
+//       url: `${process.env.NEXT_PUBLIC_BASE_URL}/about`,
+//       lastModified: new Date(),
+//     },
+//     {
+//       url: `${process.env.NEXT_PUBLIC_BASE_URL}/service`,
+//       lastModified: new Date(),
+//     },
+//     {
+//       url: `${process.env.NEXT_PUBLIC_BASE_URL}/portfolio`,
+//       lastModified: new Date(),
+//     },
+//     {
+//       url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog`,
+//       lastModified: new Date(),
+//     },
+//     {
+//       url: `${process.env.NEXT_PUBLIC_BASE_URL}/contact`,
+//       lastModified: new Date(),
+//     },
+//     ...blogUrls,
+//   ];
+// }
